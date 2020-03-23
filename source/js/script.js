e4f8d7968adbde0b8cffff43525d99224cae3374
Vadim <sakeyev@mail.ru>
2020-03-23 21:36:33 +0600

    add swiper

--- source/js/script.js
+++ source/js/script.js
@@ -34,3 +34,89 @@ function openProgramm(evt, progName) {
 
 document.getElementById('defaultOpen').click();
 /* Programms Tabs END*/
+
+// var swiper = new Swiper(".swiper-container", {
+//   pagination: {
+//     el: ".swiper-pagination"
+//   },
+//   loop: true,
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 20,
+
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       slidesPerView: 3,
+//       spaceBetween: 30
+//     },
+//     // when window width is >= 640px
+//     767: {
+//       slidesPerView: 5
+//     }
+//   }
+// });
+
+
+// basic slider prefferences
+
+var swiper = Swiper;
+var init = false;
+
+function swiperMode() {
+  var mobile = window.matchMedia('(min-width: 0px) and (max-width: 1200px)');
+  var desktop = window.matchMedia('(min-width: 1201px)');
+
+  if (mobile.matches) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper('.slider', {
+        spaceBetween: 0,
+        slidesPerView: 1,
+        wrapperClass: 'slider__list',
+        slideClass: 'slider__item',
+        slideActiveClass: 'slider__item--active',
+        slideNextClass: 'slider__next',
+        slidePrevClass: 'slider__prev',
+        slideDuplicatePrevClass: 'slider__duplicate-prev',
+        slideDuplicateNextClass: 'slider__duplicate-next',
+        loop: true,
+        pagination: {
+          el: '.slider__bullets',
+          type: 'bullets',
+          bulletElement: 'button',
+          bulletClass: 'slider__bullet',
+          bulletActiveClass: 'slider__bullet--active',
+          clickable: true
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40
+          }
+        }
+      });
+    }
+  } else if (desktop.matches) {
+    swiper.destroy();
+    init = false;
+  }
+}
+
+window.addEventListener('load', function () {
+  swiperMode();
+});
+
+window.addEventListener('resize', function () {
+  swiperMode();
+});
