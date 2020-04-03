6711e20b89488ca84634c605832f0e064f17b388
Vadim <sakeyev@mail.ru>
2020-04-03 23:19:15 +0600

    fixes bugs part 1

--- build/js/script.js
+++ build/js/script.js
@@ -84,7 +84,11 @@ function swiperMode() {
           },
           768: {
             slidesPerView: 3,
-            spaceBetween: 40
+            spaceBetween: 30
+          },
+          960: {
+            slidesPerView: 4,
+            spaceBetween: 30
           }
         }
       });
