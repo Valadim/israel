9f26cf07b83ba31a776aec64f0a58b1b8b723061
Vadim <sakeyev@mail.ru>
2020-04-09 22:41:45 +0600

    added responsive

--- build/js/script.js
+++ build/js/script.js
@@ -80,15 +80,15 @@ function swiperMode() {
           },
           480: {
             slidesPerView: 2,
-            spaceBetween: 30
+            spaceBetween: 20
           },
           768: {
             slidesPerView: 3,
-            spaceBetween: 30
+            spaceBetween: 20
           },
           960: {
             slidesPerView: 4,
-            spaceBetween: 30
+            spaceBetween: 20
           }
         }
       });
